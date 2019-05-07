106821a7b50724bc050d503d97d5165b107a989f
SPGoding <SPGoding@outlook.com>
2019-05-08 00:27:30 +0800

    :construction:Work in progress.

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,127 @@
 import * as rp from 'request-promise-native'
-import { loadConfig } from './utils'
-import { fileURLToPath } from 'url';
+import * as http from 'http'
+import { loadConfig, Config, Users, RankElement, getUserViaWebCode, writeConfig, getBBCodeOfTable, Table, Row } from './utils'
 
-interface Config {
-    password: string,
-    interal: number
-}
+let config: Config = { password: '', interal: NaN, port: NaN }
+let users: Users = {}
+let rank: RankElement[]
 
-interface User {
-    uid: number,
-    username: string,
-    heartInitial: number,
-    heartAbandoned: number,
-    heartAbandonedLinks: string[],
-    heartPresent: number,
-    heartAttained: number,
-    banned: boolean
-}
+let rankBBCode = ''
+let rankTime = ''
+let registrationBBCode = ''
+let abandonedHeartBBCode = '';
 
-let config: Config
-let users: User[]
+(async function startup() {
+    config = await loadConfig<Config>('config.json', { password: '', interal: 600000, port: 80 })
+    users = await loadConfig<Users>('users.json', {})
 
-    ; (async function startup() {
-        config = await loadConfig<Config>('config.json', { password: '', interal: 600000 })
-        users = await loadConfig<User[]>('users.json', [])
+    await updateInfo()
+    setInterval(updateInfo, config.interal)
 
-        setInterval(updateInfo, config.interal)
-    })()
+    http
+        .createServer(async (req, res) => {
+            if (req.url) {
+                res.setHeader('Content-Type', "image/png")
+                let html = '233'
+                res.end(html)
+            }
+        })
+        .listen(config.port)
+        .on('error', e => {
+            console.error(e.message)
+        })
+})()
+
 
 async function updateInfo() {
-    for (const user of users) {
+    await updateUserInfo()
+    updateRankInfo()
+    rankBBCode = drawRankTable()
+    registrationBBCode = getRegistrationBBCode()
+    abandonedHeartBBCode = getAbandonedHeartBBCode()
+    console.log(rankBBCode)
+    console.log(registrationBBCode)
+    console.log(abandonedHeartBBCode)
+}
+
+async function updateUserInfo() {
+    for (const uid in users) {
         try {
+            const user = users[uid]
             if (!user.banned) {
-                const url = `http://www.mcbbs.net/?${user.uid}`
-                const webCode = rp(url)
-                
+                const url = `http://www.mcbbs.net/?${uid}`
+                const webCode: string = await rp(url)
+                const userUpdated = getUserViaWebCode(webCode, user)
+                users[uid] = userUpdated
             }
-        }
-        catch (e) {
+        } catch (e) {
             console.error(`Updating info error: '${e}'.`)
         }
     }
+    writeConfig('users.json', users)
+}
+
+function updateRankInfo() {
+    rank = []
+    for (const uid in users) {
+        const user = users[uid]
+        rank.push({ uid: parseInt(uid), heart: user.heartPresent })
+    }
+    rank.sort((a: RankElement, b: RankElement) => a.heart - b.heart)
+    rankTime = new Date().toLocaleString()
+}
+
+function drawRankTable() {
+    const table: Table = [
+        ['[b]排名[/b]', '[b]用户名[/b]', '[b]当前获取爱心[/b]']
+    ]
+    let row: Row = []
+
+    for (const ele of rank.slice(0, 10)) {
+        row.push(rank.indexOf(ele) + 1, users[ele.uid].username, ele.heart)
+    }
+
+    return getBBCodeOfTable(table)
+}
+
+function getRegistrationBBCode() {
+    const table: Table = []
+    let row: Row = [
+        '[b]用户名[/b]', '[b]UID[/b]', '[color=Purple][b]爱心数量[/b][/color]',
+        '[b]用户名[/b]', '[b]UID[/b]', '[color=Purple][b]爱心数量[/b][/color]'
+    ]
+
+    for (const uid in users) {
+        const user = users[uid]
+
+        if (row.length >= 6) {
+            table.push(row)
+            row = []
+        }
+
+        row = [...row, user.username, uid, user.heartInitial]
+    }
+    table.push(row)
+
+    return getBBCodeOfTable(table)
+}
+
+function getAbandonedHeartBBCode() {
+    const table: Table = [
+        ['[b]用户名[/b]', '[b]UID[/b]', '[color=Red][b]放弃数量[/b][/color]', '[b]相关链接[/b]']
+    ]
+    let row: Row = []
+
+    for (const uid in users) {
+        const user = users[uid]
+        if (user.heartAbandoned) {
+            row = [
+                user.username, uid, user.heartAbandoned,
+                user.heartAbandonedLinks.map(v => `[url=${v}]最佳申请帖[/url]`).join('\n')
+            ]
+            table.push(row)
+        }
+    }
+
+    return getBBCodeOfTable(table)
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,34 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
+export interface Config {
+    password: string,
+    interal: number,
+    port: number
+}
+
+export interface User {
+    username: string,
+    heartInitial: number,
+    heartAbandoned: number,
+    heartAbandonedLinks: string[],
+    heartPresent: number,
+    heartAttained: number,
+    banned: boolean
+}
+
+export interface Users {
+    [uid: number]: User
+}
+
+export interface RankElement {
+    uid: number,
+    heart: number
+}
+
+export type Table = Row[]
+export type Row = (string | number)[]
+
 export async function loadConfig<T>(fileName: string, def?: T): Promise<T> {
     const filePath = path.join(__dirname, fileName)
 
@@ -11,4 +39,56 @@ export async function loadConfig<T>(fileName: string, def?: T): Promise<T> {
     const config = fs.readJson(filePath, { encoding: 'utf8' })
 
     return config
+}
+
+export async function writeConfig<T extends object>(fileName: string, content: T) {
+    const filePath = path.join(__dirname, fileName)
+
+    fs.writeJson(filePath, content)
+}
+
+function getStringBetweenStrings(input: string, start: string, end: string) {
+    return input.slice(input.indexOf(start) + start.length, input.indexOf(end))
+}
+
+export function getUserViaWebCode(webCode: string, oldUser?: User): User {
+    const username = getStringBetweenStrings(webCode, '<title>', '的个人资料 -  Minecraft(我的世界)中文论坛 - </title>')
+    const heartPresent = parseInt(getStringBetweenStrings(webCode, '<li><em>爱心</em>', ' 心</li>'))
+
+    if (!oldUser) {
+        oldUser = {
+            banned: false,
+            heartAbandoned: 0,
+            heartAbandonedLinks: [],
+            heartAttained: 0,
+            heartInitial: heartPresent,
+            heartPresent: NaN,
+            username: ''
+        }
+    }
+
+    let { banned, heartAbandoned, heartAbandonedLinks, heartAttained, heartInitial } = oldUser
+
+    return {
+        username,
+        heartInitial,
+        heartAbandoned,
+        heartAbandonedLinks,
+        heartPresent,
+        heartAttained: heartPresent - heartInitial - heartAbandoned,
+        banned
+    }
+}
+
+export function getBBCodeOfTable(table: Table) {
+    let ans = '[table]\n'
+    for (const row of table) {
+        ans += '[tr]'
+        for (const element of row) {
+            ans += `[td]${element}[/td]`
+        }
+        ans += '[/tr]\n'
+    }
+    ans += '[/table]'
+    return ans
 }
